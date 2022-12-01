17bc0274ec261ee4f98aa61ed4a46b483f6bf322
Jesus Quintero <jaqs2602@gmail.com>
2022-12-01 18:26:08 -0300

    Match update

--- src/components/League.jsx
+++ src/components/League.jsx
@@ -15,32 +15,32 @@ const League = () => {
       <div className="league">
         <LeagueName
           name="Shoali"
-          points="35pts"
+          points="36pts"
           medal="https://cdn0.iconfinder.com/data/icons/sport-balls/512/gold_medal.png"
         />
         <LeagueName
           name="Lule"
-          points="31pts"
+          points="32pts"
           medal="https://cdn0.iconfinder.com/data/icons/sport-balls/512/silver_medal.png"
         />
         <LeagueName
           name="Chuchantrio"
-          points="28pts"
+          points="29pts"
           medal="https://cdn0.iconfinder.com/data/icons/sport-balls/512/bronze_medal.png"
         />
         <LeagueName
           name="Lauri"
-          points="27pts"
+          points="28pts"
           medal="https://cdn.shopify.com/s/files/1/0596/9399/0049/files/Untitled-1_0002_Layer-8.png"
         />
         <LeagueName
           name="Nano"
-          points="27pts"
+          points="28pts"
           medal="https://www.meme-arsenal.com/memes/ff4b2dea60a357faa706e4b2383a2a9b.jpg"
         />
         <LeagueName
           name="Jeezy"
-          points="25pts"
+          points="26pts"
           medal="https://static-cdn.jtvnw.net/jtv_user_pictures/02191ca0-0cf9-47b6-87c5-1933b54bd964-profile_image-300x300.png"
         />
       </div>
